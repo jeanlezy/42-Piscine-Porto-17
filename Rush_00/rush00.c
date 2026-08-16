dd0b781c63ca71dd340b7d39fc21512acfcf7b01
Jeanlezy B. Sousa <78985980+jeanlezy@users.noreply.github.com>
2026-08-16 18:30:05 +0100

    Refactor print function by removing braces
    Removed unnecessary braces for single statements in print functions.

--- Rush_00/rush00.c
+++ Rush_00/rush00.c
@@ -21,17 +21,11 @@ void	print(int col, char fst, char mid, char last)
 	while(i <= col)
 	{
 		if(i == 1)
-		{
 			ft_putchar(fst);
-		}
 		else if(i == col)
-		{
 			ft_putchar(last);
-		}
 		else
-		{
 			ft_putchar(mid);
-		}
 		i++;
 	}
 	ft_putchar('\n');
@@ -42,20 +36,14 @@ void	rush(int x, int y)
 	int lin;
 	
 	if (x < 1 || y < 1)
-	{
 		return ;
-	}
 	lin = 1;
 	while(lin <= y)
 	{
 		if(lin == 1 || lin == y)
-		{
 			print(x, 'o', '-', 'o');
-		}
 		else
-		{
 			print(x, '|', ' ', '|');
-		}
 		lin++;
 	}
 }
